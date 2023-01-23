07471dd27333160bbe3af4ff9bef1ba3b3848470
DhavalV101 <dhavalvaidya.int@delhivery.com>
2023-01-23 14:59:34 +0530

    The final commit after merging all

---
+++ newCall.py
@@ -0,0 +1,1 @@
+print("I am Optimus Prime, the leader of the Autobots and we will fight till the end")
---
+++ prime.py
@@ -0,0 +1,1 @@
+print("This is Optimus Prime and I call to all the Autobots living among the stars.")
